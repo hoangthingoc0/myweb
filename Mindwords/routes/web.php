e8567e74e497aafe3372eee430a9ce62195e6abd
Hoang Thi Ngoc <23010156@st.phenikaa-uni.edu.vn>
2025-10-10 17:02:53 +0000

    v

--- Mindwords/routes/web.php
+++ Mindwords/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
 Route::get('/users', [UserController::class, 'displayUser']);
 Route::get('/users/edit/{id}', [UserController::class, 'editUser']);
 Route::post('/users/save', [UserController::class, 'saveUser']);
