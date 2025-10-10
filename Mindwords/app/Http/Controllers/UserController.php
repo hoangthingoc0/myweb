19d3e9689eafb4a65dd5746ee34b7cfed9318166
Hoang Thi Ngoc <23010156@st.phenikaa-uni.edu.vn>
2025-10-10 17:53:08 +0000

    hiển thị ra view (ds, from edit)

--- Mindwords/app/Http/Controllers/UserController.php
+++ Mindwords/app/Http/Controllers/UserController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function displayUser()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
+    public function editUser($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.edit', compact('user'));
+    }
+
+    public function saveUser(Request $request)
+    {
+        $user = User::findOrFail($request->id);
+        $user->update($request->all());
+        return redirect('/users')->with('success', 'Cập nhật thành công');
+    }
 }
