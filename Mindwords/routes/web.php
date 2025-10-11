6019d68f72b5c186ecfd9e153a9fd33811bca402
Hoang Thi Ngoc <23010156@st.phenikaa-uni.edu.vn>
2025-10-11 14:25:20 +0000

    view/home

--- Mindwords/routes/web.php
+++ Mindwords/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 use App\Http\Controllers\Api\UserController;
 Route::get('/users', [UserController::class, 'displayUser']);
