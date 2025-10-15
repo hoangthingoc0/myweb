b985fe6b6390e440e760ab9318b9ca5f9216dbe2
Hoang Thi Ngoc <23010156@st.phenikaa-uni.edu.vn>
2025-10-15 12:18:24 +0000

    register

--- Mindwords/resources/views/auth/register.blade.php
+++ Mindwords/resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Ký</title>
-    <link rel="stylesheet" href="{{ asset('css/auth.css?v=4') }}">
+    <link rel="stylesheet" href="/css/auth.css">
 </head>
 <body>
     <div class="login-container">
